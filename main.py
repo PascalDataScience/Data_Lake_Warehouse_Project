7f369643b4bcccd99f0481ce968ab15a45682fc2
Pascal Schoch <pasc.schoch@gmail.com>
2022-10-20 09:27:58 +0200

    first commit

--- main.py
+++ main.py
@@ -22,4 +22,6 @@ if __name__ == '__main__':
     df = client.query_generation(country_code, start=start, end=end)
     df2 = client.query_generation_forecast(country_code, start=start, end=end)
 
+    #hello guys
+
     #df_prices =
